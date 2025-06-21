2a23e92818bba83fd9b582b84f10b5cfa2afc94d
isabella-fc <20240685@novaims.unl.pt>
2025-06-21 17:38:28 +0100

    fix data unit tests

--- src/mlops_project/pipelines/data_units_tests/pipeline.py
+++ src/mlops_project/pipelines/data_units_tests/pipeline.py
@@ -5,7 +5,7 @@ def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
             func=data_unit_tests,
-            inputs=["ingested_data", "expectation_suite"],
+            inputs="ingested_data",
             outputs="validation_report",
             name="data_unit_tests_node"
         )
